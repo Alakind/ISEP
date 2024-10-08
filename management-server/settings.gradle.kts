4b819cd2f3f7e98432ac387d22e1f15512a0c368
s2516268 <e.a.devree@student.utwente.nl>
2024-10-08 11:46:11 +0200

    clean up management-server/build.gradle.kts

--- management-server/settings.gradle.kts
+++ management-server/settings.gradle.kts
@@ -1,7 +1,1 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
-}
 rootProject.name = "management-server"
-include("src:test:ut.isep.management")
-findProject(":src:test:ut.isep.management")?.name = "ut.isep.management"
-include("test")
