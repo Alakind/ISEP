7b0767770007f44f9213b3894da36a26519d7cf7
s2516268 <e.a.devree@student.utwente.nl>
2024-10-17 12:24:52 +0200

    Add gradle config for depending on shared models from mavenLocal()

--- applicant-server/build.gradle.kts
+++ applicant-server/build.gradle.kts
@@ -22,6 +22,7 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+	implementation("ut.isep.interview:shared-models:1.0.0")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
