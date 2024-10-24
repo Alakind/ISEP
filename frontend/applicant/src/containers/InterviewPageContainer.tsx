368113b8cbaaef5481f0b04eb990714cb7e9c438
Render <renderXR@gmail.com>
2024-10-24 20:01:52 +0200

    Added header, main, footer html + styling for main interview page.

--- frontend/applicant/src/containers/InterviewPageContainer.tsx
+++ frontend/applicant/src/containers/InterviewPageContainer.tsx
@@ -1,41 +1,58 @@
-import Assignment from "../components/Assignment";
 import InterviewHeader from "../components/InterviewHeader";
+import InterviewMain from "../components/InterviewMain";
+import InterviewFooter from "../components/InterviewFooter";
 import { AssignmentTypes } from "../utils/constants";
+import "../styles/dark_mode.css";
 
 function InterviewPageContainer() {
-  const section = {
-    assignments: [
+  const interview = {
+    sections: [
       {
-        id: "randomid12345678",
-        type: AssignmentTypes.MULTIPLE_CHOICE,
-        isSolved: false,
-        text: ["What will I get if I will sum 2 and 2?"],
-        options: ["42", "Isaac Newton", "Madagaskar"],
+        assignments: [
+          {
+            id: "randomid12345678",
+            type: AssignmentTypes.MULTIPLE_CHOICE,
+            isSolved: false,
+            text: ["What will I get if I will sum 2 and 2?"],
+            options: ["42", "Isaac Newton", "Madagaskar"],
+          },
+          {
+            id: "randomid4398630",
+            type: AssignmentTypes.OPEN,
+            isSolved: false,
+            text: [
+              "Write a 3000 words essay about Pepin the Short's conquests of the Rousillon.",
+            ],
+          },
+        ],
       },
       {
-        id: "randomid4398630",
-        type: AssignmentTypes.OPEN,
-        isSolved: false,
-        text: [
-          "Write a 3000 words essay about Pepin the Short's conquests of the Rousillon.",
+        assignments: [
+          {
+            id: "randomid12345",
+            type: AssignmentTypes.MULTIPLE_CHOICE,
+            isSolved: false,
+            text: ["What is the square root of -1?"],
+            options: ["1", "0", "imagine it would have a number"],
+          },
+          {
+            id: "randomid42069",
+            type: AssignmentTypes.OPEN,
+            isSolved: false,
+            text: ["Proof the mathetical problem about P versus NP in 2 pages"],
+          },
         ],
       },
     ],
   };
-
   return (
-    <>
-      <h1>Interview page!</h1>
-      <InterviewHeader section={section} />
-      <div>
-        {section.assignments.map((assignment) => (
-          <div key={assignment.id}>
-            <Assignment assignment={assignment} />
-          </div>
-        ))}
-      </div>
-      <div>Footer</div>
-    </>
+    <div className="page">
+      <InterviewHeader interview={interview} />
+
+      <InterviewMain interview={interview} />
+
+      <InterviewFooter />
+    </div>
   );
 }
 
