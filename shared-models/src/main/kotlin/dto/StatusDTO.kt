ddc9a2dd444312aeb4fd0d8641988398ef8fd990
s2516268 <e.a.devree@student.utwente.nl>
2024-10-25 08:47:24 +0200

    Add description to StatusDTO

--- shared-models/src/main/kotlin/dto/StatusDTO.kt
+++ shared-models/src/main/kotlin/dto/StatusDTO.kt
@@ -1,5 +1,8 @@
 package dto
 
+import io.swagger.v3.oas.annotations.media.Schema
+
+@Schema(description = "A mapping of section ids to a boolean indicating whether they have been completed")
 data class StatusDTO (
     val status: Map<Int, Boolean> //Section ID, Completed?
 )
