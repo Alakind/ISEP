956a6e150ca796a446bd7e4e8b05dd4a216e700c
s2516268 <e.a.devree@student.utwente.nl>
2024-11-06 15:21:33 +0100

    add Jackson kotlin module

--- management-server/build.gradle.kts
+++ management-server/build.gradle.kts
@@ -22,7 +22,14 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.5")
+    implementation("org.springframework.boot:spring-boot-starter-data-redis:3.3.5")
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
     implementation(project(":shared-models"))
+
+//    developmentOnly("org.springframework.boot:spring-boot-docker-compose")
+    runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
