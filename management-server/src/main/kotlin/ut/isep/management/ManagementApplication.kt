756a3b4b9b21aa902e5996cf8e9de6337cc801ce
s2516268 <e.a.devree@student.utwente.nl>
2024-11-06 15:23:07 +0100

    add Redis and JPA config

--- management-server/src/main/kotlin/ut/isep/management/ManagementApplication.kt
+++ management-server/src/main/kotlin/ut/isep/management/ManagementApplication.kt
@@ -1,13 +1,23 @@
 package ut.isep.management
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.data.redis.RedisConnectionDetails
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories
 
 @OpenAPIDefinition
 @SpringBootApplication
-class ManagementApplication
+@EnableRedisRepositories(basePackages = ["ut.isep.management.repository.redis",])
+@EnableJpaRepositories(basePackages = ["ut.isep.management.repository.pgsql",])
+class ManagementApplication {
+}
 
 fun main(args: Array<String>) {
     runApplication<ManagementApplication>(*args)
+
 }
---
+++ management-server/src/test/kotlin/ut/isep/management/PostgreSQLRepositoryTests.kt
@@ -0,0 +1,27 @@
+package ut.isep.management
+
+import enumerable.ApplicantStatus
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
+import ut.isep.management.model.pgsql.Applicant
+import ut.isep.management.repository.pgsql.ApplicantRepository
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@ExtendWith(SpringExtension::class)
+@DataJpaTest
+class PostgreSQLRepositoryTests @Autowired constructor(
+    private val applicantRepository: ApplicantRepository
+) {
+
+    @Test
+    fun `test repository connection to PostgreSQL`() {
+        val entity = applicantRepository.save(
+            Applicant(status = ApplicantStatus.app_finished, interview = null, preferredLanguage = null)
+        )
+        assert(true)
+    }
+}
