ef82219e6e628733bb24ac52f0bc41a194f22202
Alexander Ivakin <a.ivakin@student.utwente.nl>
2024-11-07 11:13:09 +0100

    WIP: getting sections

--- frontend/applicant/src/containers/InterviewPageContainer.tsx
+++ frontend/applicant/src/containers/InterviewPageContainer.tsx
@@ -1,4 +1,5 @@
-import Assignment from "../components/Assignment";
+import { useEffect, useState } from "react";
+
 import InterviewHeader from "../components/InterviewHeader";
 import InterviewMain from "../components/InterviewMain";
 import InterviewFooter from "../components/InterviewFooter";
@@ -6,51 +7,78 @@ import { AssignmentTypes } from "../utils/constants";
 import "../styles/dark_mode.css";
 
 function InterviewPageContainer() {
-  const interview = {
-    sections: [
-      {
-        assignments: [
-          {
-            id: "randomid12345678",
-            type: AssignmentTypes.MULTIPLE_CHOICE,
-            isSolved: false,
-            text: ["What will I get if I will sum 2 and 2?"],
-            options: ["42", "Isaac Newton", "Madagaskar"],
-          },
-          {
-            id: "randomid4398630",
-            type: AssignmentTypes.OPEN,
-            isSolved: false,
-            text: [
-              "Write a 3000 words essay about Pepin the Short's conquests of the Rousillon.",
-            ],
-          },
-        ],
-      },
-      {
-        assignments: [
-          {
-            id: "randomid12345",
-            type: AssignmentTypes.MULTIPLE_CHOICE,
-            isSolved: false,
-            text: ["What is the square root of -1?"],
-            options: ["1", "0", "imagine it would have a number"],
-          },
-          {
-            id: "randomid42069",
-            type: AssignmentTypes.OPEN,
-            isSolved: false,
-            text: ["Proof the mathetical problem about P versus NP in 2 pages"],
-          },
-        ],
-      },
-    ],
-  };
+  const [interview, setInterview] = useState({ sections: [] });
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const API_URL = import.meta.env.VITE_API_APPLICANT_URL + "/section";
+
+    // Fetch data from API
+    fetch(API_URL)
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        setInterview(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setIsLoading(false);
+      });
+  }, []);
+
+  console.log(interview);
+
+  //   const interview = {
+  //     sections: [
+  //       {
+  //         assignments: [
+  //           {
+  //             id: "randomid12345678",
+  //             type: AssignmentTypes.MULTIPLE_CHOICE,
+  //             isSolved: false,
+  //             text: ["What will I get if I will sum 2 and 2?"],
+  //             options: ["42", "Isaac Newton", "Madagaskar"],
+  //           },
+  //           {
+  //             id: "randomid4398630",
+  //             type: AssignmentTypes.OPEN,
+  //             isSolved: false,
+  //             text: [
+  //               "Write a 3000 words essay about Pepin the Short's conquests of the Rousillon.",
+  //             ],
+  //           },
+  //         ],
+  //       },
+  //       {
+  //         assignments: [
+  //           {
+  //             id: "randomid12345",
+  //             type: AssignmentTypes.MULTIPLE_CHOICE,
+  //             isSolved: false,
+  //             text: ["What is the square root of -1?"],
+  //             options: ["1", "0", "imagine it would have a number"],
+  //           },
+  //           {
+  //             id: "randomid42069",
+  //             type: AssignmentTypes.OPEN,
+  //             isSolved: false,
+  //             text: ["Proof the mathetical problem about P versus NP in 2 pages"],
+  //           },
+  //         ],
+  //       },
+  //     ],
+  //   };
+
   return (
     <div className="page">
       <InterviewHeader interview={interview} />
 
-      <InterviewMain interview={interview} />
+      <InterviewMain interview={interview} isLoading={isLoading} />
 
       <InterviewFooter />
     </div>
