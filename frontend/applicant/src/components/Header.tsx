465b5f129b0d90bff418ac042bec30a5c8c44868
JSweers <j.e.sweers@student.utwente.nl>
2024-11-14 16:54:15 +0100

    Implemented dynamic header elements for the current section number and removed the progress bar

--- frontend/applicant/src/components/Header.tsx
+++ frontend/applicant/src/components/Header.tsx
@@ -1,29 +1,16 @@
 import { AssessmentInterface } from "../utils/types";
 import "../styles/dark_mode_header.css";
 
-function Header({ assessment }: Props) {
+function Header({ assessment, currentSectionIndex }: Props) {
   return (
     <header className="header">
-      {/* <div>Number of assignments: {section.assignments.length}</div> */}
-      {/* //TODO fix dummy data. 1 should be current section */}
       <span className="header__left">
         <div>
-          Section 1 of {assessment.sections.length}:
+          Section {currentSectionIndex + 1} of {assessment.sections.length}:
         </div>
         <div>
           {/* TODO make section types a variable */}
-          Multiple Choice Questions
-        </div>
-        <div>
-          <div
-            className="progress header__progress"
-            role="progressbar"
-            aria-valuenow={50}
-            aria-valuemin={0}
-            aria-valuemax={100}
-          >
-            <div className="header__progress-bar"></div>
-          </div>
+          {assessment.sections[currentSectionIndex].name} Questions
         </div>
       </span>
       <span className="header__right">
@@ -41,6 +28,7 @@ function Header({ assessment }: Props) {
 
 interface Props {
   assessment: AssessmentInterface;
+  currentSectionIndex: number;
 }
 
 export default Header;
