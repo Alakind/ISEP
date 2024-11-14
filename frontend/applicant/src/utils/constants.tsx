e5613b68c590060cc08c9bc831787571d2b0f1d6
ArtemisAndApollo <113710099+ArtemisAndApollo@users.noreply.github.com>
2024-11-14 11:39:17 +0100

    Update constants.tsx
    Removed AssignmentTypesBackend because not needed

--- frontend/applicant/src/utils/constants.tsx
+++ frontend/applicant/src/utils/constants.tsx
@@ -3,9 +3,3 @@ export const AssignmentTypes = {
   MULTIPLE_CHOICE: "MultipleChoice",
   OPEN: "Open",
 };
-
-export const AssignmentTypesBackend = {
-  CODING: "Coding",
-  MULTIPLE_CHOICE: "MultipleChoice",
-  OPEN: "Open",
-};
