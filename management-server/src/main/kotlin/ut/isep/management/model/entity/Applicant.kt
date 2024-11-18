6b5a14ad1b96dd4355bd68b3e5adfa9157b79fa2
s2516268 <e.a.devree@student.utwente.nl>
2024-11-18 12:11:11 +0100

    set entity.Applicant fields to var so they can be updated in PUT

--- management-server/src/main/kotlin/ut/isep/management/model/entity/Applicant.kt
+++ management-server/src/main/kotlin/ut/isep/management/model/entity/Applicant.kt
@@ -5,13 +5,13 @@ import jakarta.persistence.*
 import java.util.*
 
 @Entity
-class Applicant(
+open class Applicant(
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    val id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000"),
-    val status: ApplicantStatus = ApplicantStatus.not_started,
+    var id: UUID = UUID.fromString("00000000-0000-0000-0000-000000000000"),
+    var status: ApplicantStatus = ApplicantStatus.not_started,
+    var preferredLanguage: String? = null,
+
     @ManyToOne(cascade = [CascadeType.ALL])
-    @JoinColumn(name = "interview_id")
-    val interview: Interview? = null,
-    val preferredLanguage: String? = null,
+    var invite: Invite? = null
 )
