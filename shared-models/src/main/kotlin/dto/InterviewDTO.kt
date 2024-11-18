73a36372248bda854cfa226b222e8186d54a0a81
s2516268 <e.a.devree@student.utwente.nl>
2024-11-18 17:57:02 +0100

    Change InterviewDTO to only contain list of section IDs

--- shared-models/src/main/kotlin/dto/InterviewDTO.kt
+++ shared-models/src/main/kotlin/dto/InterviewDTO.kt
@@ -5,7 +5,7 @@ import io.swagger.v3.oas.annotations.media.Schema
 @Schema(description = "A test, consisting of multiple Sections")
 data class InterviewDTO(
     val id: Long?,
-    val sections: List<SectionDTO>
+    val sections: List<Long>
 ) {
     constructor(): this(null, emptyList())
 }
