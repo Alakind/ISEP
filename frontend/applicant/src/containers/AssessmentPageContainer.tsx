db4e25419751378650b3a3d2c0d2aafd306b77c6
JSweers <j.e.sweers@student.utwente.nl>
2024-11-19 10:12:25 +0100

    Update package.json dependencies and IDE caught errors with analysis.

--- frontend/applicant/src/containers/AssessmentPageContainer.tsx
+++ frontend/applicant/src/containers/AssessmentPageContainer.tsx
@@ -16,12 +16,10 @@ function AssessmentPageContainer() {
 
   useEffect(() => {
     const API_URL =  "https://localhost:8081/section"; //import.meta.env.VITE_API_APPLICANT_URL +
-    console.log(API_URL)
 
     // Fetch data from API
     fetch(API_URL)
         .then((response) => {
-            console.log(response);
           if (!response.ok) {
             throw new Error("Network response was not ok");
           }
@@ -29,7 +27,6 @@ function AssessmentPageContainer() {
         })
         .then((data) => {
           setAssessment({ sections: data });
-          console.log("data: " + data);
             for (let i = 0; i < data.length; i++) {
                 initialAssignments.push(0);
             }
@@ -37,116 +34,9 @@ function AssessmentPageContainer() {
           setIsLoading(false);
         })
         .catch((error) => {
-          console.log("error: " + error);
           setIsLoading(true);
         });
   }, []);
-
-  console.log("assessment: " + assessment);
-
-
-  // const assessment = {
-  //   sections: [
-  //     {
-  //       name: "Multiple-Choice 1",
-  //       assignments: [
-  //         {
-  //           id: "randomid12345678",
-  //           type: AssignmentTypes.MULTIPLE_CHOICE,
-  //           isSolved: false,
-  //           text: ["What is the best scouting association?"],
-  //           options: [
-  //             "Scouting St. Jozef Hooglanderveen | Vathorst",
-  //             "Radix",
-  //             "Scouting Nederland",
-  //             "Studentenscouting U.F.O.-Stam!",
-  //           ],
-  //           isMultipleAnswers : true,
-  //         },
-  //         {
-  //           id: "randomid4398630",
-  //           type: AssignmentTypes.OPEN,
-  //           isSolved: false,
-  //           text: [
-  //             "Write a 3000 words essay about Pepin the Short's conquests of the Rousillon.",
-  //           ],
-  //         },
-  //         {
-  //           id: "randomid43982345674534630",
-  //           type: AssignmentTypes.OPEN,
-  //           isSolved: false,
-  //           text: [
-  //             "Make a story about teletubbie of 5000 words.",
-  //           ],
-  //         },
-  //         {
-  //           id: "randomid439234234674534630",
-  //           type: AssignmentTypes.OPEN,
-  //           isSolved: false,
-  //           text: [
-  //             "Make a story about time of 5000 words.",
-  //           ],
-  //         },
-  //         {
-  //           id: "randomid43982345234234630",
-  //           type: AssignmentTypes.OPEN,
-  //           isSolved: false,
-  //           text: [
-  //             "Make a story about bricks of 5000 words.",
-  //           ],
-  //         },
-  //         {
-  //           id: "randomid43982343242345674534630",
-  //           type: AssignmentTypes.OPEN,
-  //           isSolved: false,
-  //           text: [
-  //             "Make a story about quantum mechanics of 5000 words.",
-  //           ],
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       name: "Multiple-Choice 2",
-  //       assignments: [
-  //         {
-  //           id: "randomid12345",
-  //           type: AssignmentTypes.MULTIPLE_CHOICE,
-  //           isSolved: false,
-  //           text: ["What is the square root of -1?"],
-  //           options: ["1", "0", "imagine it would have a number"],
-  //           isMultipleAnswers: false,
-  //         },
-  //         {
-  //           id: "randomid42069",
-  //           type: AssignmentTypes.OPEN,
-  //           isSolved: false,
-  //           text: ["Proof the mathetical problem about P versus NP in 2 pages"],
-  //         },
-  //       ],
-  //     },
-  //     {
-  //       name: "Java",
-  //       assignments: [
-  //         {
-  //           id: "codingAss1",
-  //           type: AssignmentTypes.CODING,
-  //           isSolved: false,
-  //           text: "What is the following called?",
-  //           image: "",
-  //         },
-  //         {
-  //           id: "codingAss2",
-  //           type: AssignmentTypes.CODING,
-  //           isSolved: false,
-  //           text: "Extend the following code?",
-  //           image: "",
-  //         }
-  //       ],
-  //     },
-  //   ],
-  // };
-
-
 
   return (
     <div className="page">
