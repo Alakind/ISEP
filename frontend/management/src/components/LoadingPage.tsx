be7994154432e79ffeafe1c7eedc96c087b2a017
ArtemisAndApollo <113710099+ArtemisAndApollo@users.noreply.github.com>
2024-11-24 17:51:43 +0100

    Setup of starting components and structure (#22)

---
+++ frontend/management/src/components/LoadingPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactLoading from 'react-loading';
+
+function LoadingPage({errorMessage} : Props ) {
+    return (
+        <div className="page page--center">
+            <ReactLoading type="spin" color='var(--text-primary)' height={'80px'} width={'80px'} />
+            <br></br>
+            <p>
+                {errorMessage != null ? errorMessage.toString() : "Loading..."}
+            </p>
+        </div>
+    )
+}
+
+export interface Props {
+    errorMessage?: string;
+}
+
+export default LoadingPage
--- frontend/management/src/main.tsx
+++ frontend/management/src/main.tsx
@@ -1,21 +1,167 @@
-import { StrictMode } from "react";
+import {StrictMode, Suspense} from "react";
 import { createRoot } from "react-dom/client";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 
 import "./index.css";
+import 'react-toastify/dist/ReactToastify.css';
 import ApplicantsListContainer from "./containers/ApplicantsListContainer";
 import ApplicantPageContainer from "./containers/ApplicantPageContainer";
+import PageNotFoundContainer from "./containers/PageNotFoundContainer.tsx";
+import LoadingPage from "./components/LoadingPage.tsx";
+import ErrorBoundary from "./components/ErrorBoundary.tsx";
 
 const router = createBrowserRouter([
   {
-    path: "/applicants",
-    element: <ApplicantsListContainer />,
+    path: "/",
+    children: [
+      {
+        path: "login",
+        element: (
+          <Suspense fallback={<LoadingPage />}>
+            <>/login</>
+          </Suspense>
+        ),
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+      {
+        path: "profile",
+        element: (
+            <Suspense fallback={<LoadingPage />}>
+                <>/profile</>
+            </Suspense>
+        ),
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+      {
+        path: "assessments",
+        element: (
+            <Suspense fallback={<LoadingPage />}>
+                <>/assessments</>
+            </Suspense>
+        ),
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+      {
+        path: "users",
+        element: (
+            <Suspense fallback={<LoadingPage />}>
+                <>/users</>
+            </Suspense>
+        ),
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+      {
+        path: "user/:id/info",
+        element: (
+            <Suspense fallback={<LoadingPage />}>
+                <>/user/:id/info</>
+            </Suspense>
+        ),
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+      {
+        path: "applicants",
+        element: (
+          <Suspense fallback={<LoadingPage />}>
+              <ApplicantsListContainer />
+          </Suspense>
+        ),
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+      {
+        path: "applicant",
+        children: [
+          {
+            path: "add",
+            element: (
+                <Suspense fallback={<LoadingPage />}>
+                    <>/applicant/add</>
+                </Suspense>
+            ),
+            errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+          },
+          {
+            path: ":id",
+            children: [
+              {
+                path: "info",
+                element: (
+                    <Suspense fallback={<LoadingPage />}>
+                        <ApplicantPageContainer />
+                    </Suspense>
+                ),
+                errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+              },
+              {
+                path: "edit",
+                element: (
+                    <Suspense fallback={<LoadingPage />}>
+                        <>/applicant/:id/edit</>
+                    </Suspense>
+                ),
+                errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+              },
+              {
+                path: "invite",
+                children: [
+                  {
+                    path: "show",
+                    element: (
+                        <Suspense fallback={<LoadingPage />}>
+                            <>/applicant/:id/invite/show</>
+                        </Suspense>
+                    ),
+                    errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+                  },
+                  {
+                    path: "add",
+                    element: (
+                      <Suspense fallback={<LoadingPage />}>
+                          <>/applicant/:id/invite/add</>
+                      </Suspense>
+                    ),
+                    errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+                  },
+                  {
+                    path: "edit",
+                    element: (
+                      <Suspense fallback={<LoadingPage />}>
+                          <>/applicant/:id/invite/edit</>
+                      </Suspense>
+                    ),
+                    errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+                  },
+                ],
+                errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+              },
+            ],
+            errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+          },
+        ],
+        errorElement: <ErrorBoundary error={new Error("Page not found")} />,
+      },
+    ],
+    errorElement: <ErrorBoundary error={new Error("Page not found")} />,
   },
   {
-    path: "/applicants/:id",
-    element: <ApplicantPageContainer />,
+    path: "*",
+    element: (
+      <Suspense fallback={<LoadingPage />}>
+        <PageNotFoundContainer />
+      </Suspense>
+  ),
+  }
+], {
+  future: {
+    v7_relativeSplatPath: true,
+    v7_skipActionErrorRevalidation: true,
+    v7_partialHydration: true,
+    v7_normalizeFormMethod: true,
+    v7_fetcherPersist: true,
+    v7_startTransition: true,
   },
-]);
+  basename: "/management",
+});
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
