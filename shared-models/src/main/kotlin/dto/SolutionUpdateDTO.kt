efbbb8ec90c1fd4ff3be2326c893f18e5fb93049
s2516268 <e.a.devree@student.utwente.nl>
2024-12-06 13:56:54 +0100

    fix parsing assignment IDs as strings, update type property casing

--- shared-models/src/main/kotlin/dto/SolutionUpdateDTO.kt
+++ shared-models/src/main/kotlin/dto/SolutionUpdateDTO.kt
@@ -3,4 +3,4 @@ package dto
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "A mapping of answer objects to question IDs")
-class SolutionUpdateDTO : HashMap<Int, AnswerUpdateDTO>()
+class SolutionUpdateDTO : HashMap<String, AnswerUpdateDTO>()
