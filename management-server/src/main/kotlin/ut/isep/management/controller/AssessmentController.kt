f8b1adf070500e3db43eaf838dc66377045e20e8
s2516268 <e.a.devree@student.utwente.nl>
2024-12-06 16:31:31 +0100

    add endpoint for getting all assessment IDs

---
+++ management-server/src/main/kotlin/ut/isep/management/controller/AssessmentController.kt
@@ -0,0 +1,23 @@
+package ut.isep.management.controller
+
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import ut.isep.management.service.AssessmentService
+
+
+@RestController
+@RequestMapping("/asessment")
+class AssessmentController(val assessmentService: AssessmentService) {
+
+
+    @GetMapping
+    @ApiResponse(
+        responseCode = "200",
+        description = "Returns a list of all assessment IDs",
+    )
+    fun getAssessmentIDs(): List<Long> {
+       return assessmentService.allAssessmentIDs
+    }
+}
