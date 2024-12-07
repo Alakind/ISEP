3c6d30272ad64657858a4c89ba8388175d60d258
s2516268 <e.a.devree@student.utwente.nl>
2024-12-07 23:03:23 +0100

    rename Assignment DTOs

--- shared-models/src/main/kotlin/dto/assignment/AssignmentMultipleChoiceReadDTO.kt
+++ shared-models/src/main/kotlin/dto/assignment/AssignmentMultipleChoiceReadDTO.kt
@@ -4,10 +4,10 @@ import io.swagger.v3.oas.annotations.media.Schema
 
 
 @Schema(description = "Multiple choice assignment")
-data class AssignmentMultipleChoiceDTO(
+data class AssignmentMultipleChoiceReadDTO(
     override val id: Long?,
     override val type: AssignmentType = AssignmentType.MultipleChoice,
     override val description: String,
     val options: List<String>,
     val isMultipleAnswers: Boolean
-) : AssignmentDTO()
+) : AssignmentReadDTO()
