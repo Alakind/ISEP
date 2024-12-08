e7045f0f0444015d7a630bb740aad508c914813a
s2516268 <e.a.devree@student.utwente.nl>
2024-12-09 00:05:46 +0100

    remove SolutionCreateConverter.kt

--- shared-models/src/main/kotlin/dto/solution/SolvedAssignmentCreateReadDTO.kt
+++ shared-models/src/main/kotlin/dto/solution/SolvedAssignmentCreateReadDTO.kt
@@ -1,29 +1,31 @@
-package dto.assignment
+package dto.solution
 
 import com.fasterxml.jackson.annotation.JsonSubTypes
 import com.fasterxml.jackson.annotation.JsonTypeInfo
 import io.swagger.v3.oas.annotations.media.Schema
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
+import dto.CreateDTO
+import dto.ReadDTO
 
 @Schema(description = "Answer objects for open, multiple-choice, and coding questions")
 @JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.PROPERTY, property = "type")
 @JsonSubTypes(
-    JsonSubTypes.Type(value = AnswerUpdateDTO.MultipleChoiceAnswer::class, name = "MultipleChoice"),
-    JsonSubTypes.Type(value = AnswerUpdateDTO.OpenAnswer::class, name = "Open"),
-    JsonSubTypes.Type(value = AnswerUpdateDTO.CodingAnswer::class, name = "Coding")
+    JsonSubTypes.Type(value = SolvedAssignmentCreateReadDTO.MultipleChoiceAnswer::class, name = "MultipleChoice"),
+    JsonSubTypes.Type(value = SolvedAssignmentCreateReadDTO.OpenAnswer::class, name = "Open"),
+    JsonSubTypes.Type(value = SolvedAssignmentCreateReadDTO.CodingAnswer::class, name = "Coding")
 )
-sealed class AnswerUpdateDTO {
+sealed class SolvedAssignmentCreateReadDTO : CreateDTO, ReadDTO {
 
     data class MultipleChoiceAnswer @JsonCreator constructor(
         @JsonProperty("answer") val answer: List<Int>
-    ) : AnswerUpdateDTO()
+    ) : SolvedAssignmentCreateReadDTO()
 
     data class OpenAnswer @JsonCreator constructor(
         @JsonProperty("answer") val answer: String
-    ) : AnswerUpdateDTO()
+    ) : SolvedAssignmentCreateReadDTO()
 
     data class CodingAnswer @JsonCreator constructor(
         @JsonProperty("answer") val answer: String
-    ) : AnswerUpdateDTO()
+    ) : SolvedAssignmentCreateReadDTO()
 }
