1315861142338890583e037e370e30645d19fe4c
s2516268 <e.a.devree@student.utwente.nl>
2024-12-09 13:34:34 +0100

    remove GET /assessment/id endpoint, add GET /assessment, GET /assessment/{id}

--- management-server/src/main/kotlin/ut/isep/management/controller/AssessmentController.kt
+++ management-server/src/main/kotlin/ut/isep/management/controller/AssessmentController.kt
@@ -1,10 +1,16 @@
 package ut.isep.management.controller
 
+import dto.PaginatedDTO
+import dto.assessment.AssessmentReadDTO
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.boot.web.servlet.error.DefaultErrorAttributes
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import ut.isep.management.service.assessment.AssessmentReadService
 
 
@@ -14,12 +20,42 @@ import ut.isep.management.service.assessment.AssessmentReadService
 class AssessmentController(val assessmentReadService: AssessmentReadService) {
 
 
-    @GetMapping("/id")
+    @GetMapping
+    @Operation(summary = "Get all assessments", description = "Returns a list of all assessments")
     @ApiResponse(
         responseCode = "200",
-        description = "Returns a list of all assessment IDs",
+        description = "Returns a list of all assessments",
     )
-    fun getAssessmentIDs(): List<Long> {
-       return assessmentReadService.getAll().map {it.id}
+    fun getAssessments(@RequestParam(required = false) limit: Int?,
+                      @RequestParam(required = false) page: Int?,
+                      @RequestParam(required = false,) sort: String?
+    ): PaginatedDTO<AssessmentReadDTO> {
+        return assessmentReadService.getPaginated(limit, page, sort)
+    }
+
+
+    @GetMapping("{id}")
+    @Operation(summary = "Get assessment", description = "Returns an assessment or 404 if not found")
+    @ApiResponses(
+        value = [
+            ApiResponse(
+                responseCode = "200",
+                description = "Found the assessment",
+            ),
+            ApiResponse(
+                responseCode = "404",
+                description = "Assessment not found",
+                content = [Content(
+                    schema = Schema(implementation = DefaultErrorAttributes::class)
+                )]
+            )
+        ]
+    )
+    fun getAssessment(@PathVariable id: Long): ResponseEntity<AssessmentReadDTO> {
+        return try {
+            ResponseEntity.ok(assessmentReadService.getById(id))
+        } catch (e: NoSuchElementException) {
+            ResponseEntity.status(404).build()
+        }
     }
 }
