46f92eb376b056af0f51ca340711af55cde195cb
JSweers <j.e.sweers@student.utwente.nl>
2024-12-12 23:41:05 +0100

    Extract components of status overview and result viewer and moved styling

--- frontend/management/src/components/applicant-personal/ApplicantPersonalPage.tsx
+++ frontend/management/src/components/applicant-personal/ApplicantPersonalPage.tsx
@@ -1,6 +1,5 @@
 import {ApplicantInterface} from "../../utils/types.tsx";
 import "../../styles/applicant-personal-page.css"
-import StatusItem from "../StatusItem.tsx";
 import {ApplicantStatuses} from "../../utils/constants.tsx";
 import {mapStatus} from "../../utils/mapping.tsx";
 import AssessmentResultsViewerContainer from "../../containers/applicant-personal/AssessmentResultsViewerContainer.tsx";
@@ -9,6 +8,7 @@ import ApplicantPersonalCardContainer from "../../containers/applicant-personal/
 import {Dispatch, ReactNode, SetStateAction} from "react";
 import CardHeaderContainer from "../../containers/card/CardHeaderContainer.tsx";
 import CardBodyContainer from "../../containers/card/CardBodyContainer.tsx";
+import StatusOverview from "./StatusOverview.tsx";
 
 
 function ApplicantPersonalPage({applicant, setApplicant, goToApplicantsPage, assessmentId}: Props): ReactNode {
@@ -23,18 +23,11 @@ function ApplicantPersonalPage({applicant, setApplicant, goToApplicantsPage, ass
         />
       </CardHeaderContainer>
       <CardBodyContainer>
-        <span className="applicant-page__container__info">
-          <ApplicantPersonalCardContainer applicant={applicant} setApplicant={setApplicant}/>
-        </span>
-        <span className="applicant-page__container__status-overview">
-          <h4>Status</h4>
-          <StatusItem status={mapStatus(applicant.status)}/>
-        </span>
+        <ApplicantPersonalCardContainer applicant={applicant} setApplicant={setApplicant}/>
+        <StatusOverview applicant={applicant}/>
       </CardBodyContainer>
       {mapStatus(applicant.status) == (ApplicantStatuses.APP_FINISHED || ApplicantStatuses.INTERVIEW_FINISHED || ApplicantStatuses.INTERVIEW_INVITED) && applicant.invite ?
-        <div className="applicant-page__results">
-          <AssessmentResultsViewerContainer assessmentId={assessmentId} inviteUuid={applicant.invite}/>
-        </div> :
+        <AssessmentResultsViewerContainer assessmentId={assessmentId} inviteUuid={applicant.invite}/> :
         <></>
       }
     </>
--- frontend/management/src/components/applicant-personal/AssessmentResultsOverview.tsx
+++ frontend/management/src/components/applicant-personal/AssessmentResultsOverview.tsx
@@ -1,9 +1,17 @@
 import {ReactNode} from "react";
+import {AssessmentInterface} from "../../utils/types.tsx";
 
-function AssessmentResultsOverview(): ReactNode {
+function AssessmentResultsOverview({assessmentData}: Props): ReactNode {
   return (
-    <div>Coming soon</div>
+    <>
+      <div>Coming soon!</div>
+      <div>Tag: {assessmentData.tag}</div>
+    </>
   )
+}
+
+interface Props {
+  assessmentData: AssessmentInterface;
 }
 
 export default AssessmentResultsOverview
