2418a0ae92942d0a23467a0e4fa3630c536b3ab0
s2516268 <e.a.devree@student.utwente.nl>
2024-12-16 17:01:30 +0100

    add size to SectionReadDTO

--- shared-models/src/main/kotlin/dto/section/SectionReadDTO.kt
+++ shared-models/src/main/kotlin/dto/section/SectionReadDTO.kt
@@ -8,5 +8,6 @@ import io.swagger.v3.oas.annotations.media.Schema
 data class SectionReadDTO (
     val id: Long,
     val title: String,
-    val assignments: List<AssignmentReadDTO>
-) : ReadDTO
+    val assignments: List<AssignmentReadDTO>,
+    val size: Int = assignments.size
+    ) : ReadDTO
