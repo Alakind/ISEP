e90bc221be4b946258bbb291ac689478ae0c1659
s2516268 <e.a.devree@student.utwente.nl>
2024-12-16 17:09:24 +0100

    add custom matcher for user entities

--- management-server/src/main/kotlin/ut/isep/management/service/user/UserReadService.kt
+++ management-server/src/main/kotlin/ut/isep/management/service/user/UserReadService.kt
@@ -2,6 +2,7 @@ package ut.isep.management.service.user
 
 
 import dto.user.UserReadDTO
+import org.springframework.data.domain.ExampleMatcher
 import org.springframework.stereotype.Service
 import ut.isep.management.model.entity.User
 import ut.isep.management.repository.UserRepository
@@ -12,5 +13,11 @@ import ut.isep.management.service.converter.user.UserReadConverter
 class UserReadService(
     repository: UserRepository,
     converter: UserReadConverter,
-) : ReadService<User, UserReadDTO, Long>(repository, converter)
+) : ReadService<User, UserReadDTO, Long>(repository, converter) {
+    override val matcher = ExampleMatcher.matching()
+        .withIgnoreNullValues()
+        .withMatcher("name", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+        .withMatcher("email", ExampleMatcher.GenericPropertyMatchers.contains().ignoreCase())
+        .withIgnorePaths("role")
+}
 
