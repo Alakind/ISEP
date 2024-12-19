00f7061b1a9149a5fd8fcd92d6a1df2f6476ab0d
JSweers <j.e.sweers@student.utwente.nl>
2024-12-19 11:46:44 +0100

    Refactor search component/container to use search terms for the query

--- frontend/management/src/components/table/Search.tsx
+++ frontend/management/src/components/table/Search.tsx
@@ -1,22 +1,30 @@
 import "../../styles/search.css"
 import {ChangeEvent, Dispatch, ReactNode, SetStateAction} from "react";
 
-function Search({query, setQuery, clearSearch}: Props): ReactNode {
+function Search({searchKeyword, setSearchKeyword, clearSearch, selectedOption, handleSelect}: Props): ReactNode {
   return (
-    <div className="user-list__div">
-      <form className="form-inline user-list__form">
-        <input id="search" value={query} onChange={(e: ChangeEvent<HTMLInputElement>): void => setQuery(e.target.value)} className="form-control mr-sm-2 input--mod" type="text" placeholder="Search..."
+    <div className="search__div">
+      <form className="form-inline search__div__form">
+        <select className="form-select search__div__form__select" id="searchSelect" onChange={(e: ChangeEvent<HTMLSelectElement>): void => handleSelect(e)} defaultValue={selectedOption}>
+          <option value={"name"}>Name</option>
+          <option value={"email"}>Email</option>
+          <option value={"both"}>Both</option>
+        </select>
+        <input id="search" name={"search"} value={searchKeyword} onChange={(e: ChangeEvent<HTMLInputElement>): void => setSearchKeyword(e.target.value)} className="form-control input--mod" type="text"
+               placeholder="Search..."
                aria-label="Search"/>
-        <a onClick={clearSearch} className="btn btn-primary my-2 my-sm-0 btn--mod btn--search" role="button"><i className="bi bi-x-lg"></i></a>
+        <a onClick={clearSearch} className="btn btn-primary btn--search" role="button"><i className="bi bi-x-lg search__div__form__icon"></i></a>
       </form>
     </div>
   )
 }
 
 interface Props {
-  query: string;
-  setQuery: Dispatch<SetStateAction<string>>;
+  searchKeyword: string;
+  setSearchKeyword: Dispatch<SetStateAction<string>>;
   clearSearch: () => void;
+  selectedOption: string;
+  handleSelect: (e: ChangeEvent<HTMLSelectElement>) => void;
 }
 
 export default Search
