0e4a7ddf06a96d942f161d85129de13966c59204
s2516268 <e.a.devree@student.utwente.nl>
2024-12-23 16:31:25 +0100

    fix O(N^2) SectionReadService.getSolvedSection function

--- management-server/src/main/kotlin/ut/isep/management/service/section/SectionReadService.kt
+++ management-server/src/main/kotlin/ut/isep/management/service/section/SectionReadService.kt
@@ -2,10 +2,13 @@ package ut.isep.management.service.section
 
 import dto.section.SectionReadDTO
 import dto.section.SolvedSectionReadDTO
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import ut.isep.management.model.entity.Section
+import ut.isep.management.model.entity.SolvedAssignmentId
 import ut.isep.management.repository.InviteRepository
 import ut.isep.management.repository.SectionRepository
+import ut.isep.management.repository.SolvedAssignmentRepository
 import ut.isep.management.service.ReadService
 import ut.isep.management.service.converter.section.SectionReadConverter
 import ut.isep.management.service.converter.solution.SolvedAssignmentReadConverter
@@ -19,13 +22,19 @@ class SectionReadService(
     converter: SectionReadConverter,
     val solvedAssignmentReadConverter: SolvedAssignmentReadConverter,
     val inviteRepository: InviteRepository,
+    val solvedAssignmentRepository: SolvedAssignmentRepository,
 ) : ReadService<Section, SectionReadDTO, Long>(repository, converter) {
 
     fun getSolvedSection(inviteId: UUID, sectionId: Long): SolvedSectionReadDTO {
         val section = repository.findById(sectionId).orElseThrow {NoSuchElementException("No section with ID: $sectionId")}
-        val invite = inviteRepository.findById(inviteId).orElseThrow {NoSuchElementException("No invite with ID: $inviteId")}
-        // O(n^2)! TODO: look into this after presentation 2
-        val assignmentDTOs = invite.solutions.filter {it.assignment in section.assignments}.map {solvedAssignmentReadConverter.toDTO(it)}
+        if (!inviteRepository.existsById(inviteId)) {
+            throw NoSuchElementException("No invite with ID: $inviteId")
+        }
+        // Look for solved versions of all assignments of the section
+        val assignmentDTOs = section.assignments.mapNotNull { assignment ->
+            solvedAssignmentRepository.findByIdOrNull(SolvedAssignmentId(inviteId, assignment.id))
+                ?.let { solvedAssignmentReadConverter.toDTO(it) }
+        }
         return SolvedSectionReadDTO(id = sectionId, title = section.title!!, assignments = assignmentDTOs)
     }
 }
