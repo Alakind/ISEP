28168f73b9f18eea31138aaa497ef1f1e99c2c1b
JSweers <j.e.sweers@student.utwente.nl>
2024-12-31 16:24:58 +0100

    Test applicant-personal components

--- frontend/management/src/containers/card/CardBodyContainer.tsx
+++ frontend/management/src/containers/card/CardBodyContainer.tsx
@@ -1,8 +1,8 @@
 import {Children, JSXElementConstructor, ReactElement, ReactNode, ReactPortal} from 'react';
 
-function CardBodyContainer({children}: Props): ReactNode {
+function CardBodyContainer({children}: Readonly<Props>): ReactNode {
   return (
-    <div className={"card-page__body"}>
+    <div className={"card-page__body"} data-testid={"card-body-container"}>
       {Children.map(children, (child: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined): ReactNode =>
         <>
           {child}
--- frontend/management/src/containers/card/CardHeaderContainer.tsx
+++ frontend/management/src/containers/card/CardHeaderContainer.tsx
@@ -1,8 +1,8 @@
 import {Children, JSXElementConstructor, ReactElement, ReactNode, ReactPortal} from 'react'
 
-function CardHeaderContainer({children}: Props): ReactNode {
+function CardHeaderContainer({children}: Readonly<Props>): ReactNode {
   return (
-    <div className={"card-page__header"}>
+    <div className={"card-page__header"} data-testid={"card-header-container"}>
       {Children.map(children, (child: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined): ReactNode =>
         <>
           {child}
