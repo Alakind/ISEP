655f4bc56a79da7c17d7f8695099b94757782332
JSweers <j.e.sweers@student.utwente.nl>
2024-12-31 15:37:45 +0100

    Update vitest config

--- frontend/management/vitest.config.ts
+++ frontend/management/vitest.config.ts
@@ -1,7 +1,13 @@
 import {defineConfig, mergeConfig} from 'vitest/config'
 import viteConfig from './vite.config'
+import {resolve} from "path";
 
 export default mergeConfig(viteConfig, defineConfig({
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
   test: {
     environment: 'jsdom',
     globals: true,
