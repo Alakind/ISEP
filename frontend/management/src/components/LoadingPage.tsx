fa68657cfc39bbc81438a1fd0a6803a810f9bbd7
JSweers <j.e.sweers@student.utwente.nl>
2024-12-31 03:32:32 +0100

    Test LoadingPage.tsx component

--- frontend/management/src/components/LoadingPage.tsx
+++ frontend/management/src/components/LoadingPage.tsx
@@ -1,17 +1,15 @@
 import ReactLoading from 'react-loading';
 import {ReactNode} from "react";
 
-function LoadingPage({errorMessage, additionalClasses, size, showMessage}: Props): ReactNode {
+function LoadingPage({errorMessage, additionalClasses, size, showMessage}: Readonly<Props>): ReactNode {
   return (
-    <div className={`page page--center ${additionalClasses}`}>
+    <div className={`page page--center ${additionalClasses ?? ""}`}>
       <ReactLoading type="spin" color='var(--text-primary)' height={`${size ?? "80"}px`} width={`${size ?? "80"}px`}/>
       <br></br>
       {showMessage ?
-        <>
-          <p>
-            {errorMessage != null ? errorMessage.toString() : "Loading..."}
-          </p>
-        </>
+        <p>
+          {errorMessage != null ? errorMessage.toString() : "Loading..."}
+        </p>
         : <></>
       }
     </div>
