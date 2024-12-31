029488195b9d21307e7bc92a1899e0a28d0993aa
JSweers <j.e.sweers@student.utwente.nl>
2024-12-31 02:54:40 +0100

    Test Progressbar.tsx component

--- frontend/management/src/components/Progressbar.tsx
+++ frontend/management/src/components/Progressbar.tsx
@@ -2,9 +2,15 @@ import {ApplicantInterface} from "../utils/types.tsx";
 import "../styles/progressbar.css";
 import {ReactNode} from "react";
 
-function Progressbar({applicant}: Props): ReactNode {
+function Progressbar({applicant}: Readonly<Props>): ReactNode {
   return (
-    <progress id={`progressbar${applicant.id}`} max="100" value={applicant.score ? applicant.score : 0}>({applicant.score ? applicant.score : 0}%)</progress>
+    <progress
+      id={`progressbar${applicant.id}`}
+      max="100"
+      value={applicant.score ? applicant.score : 0}
+    >
+      ({applicant.score ? applicant.score : 0}%)
+    </progress>
   )
 }
 
