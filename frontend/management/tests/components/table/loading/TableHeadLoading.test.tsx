d8f382b0468becf7e264f05e9efdd041bd62004a
JSweers <j.e.sweers@student.utwente.nl>
2025-01-01 16:51:39 +0100

    Test TableHeadLoading.tsx component

---
+++ frontend/management/tests/components/table/loading/TableHeadLoading.test.tsx
@@ -0,0 +1,46 @@
+import TableHeadLoading from "../../../../src/components/table/loading/TableHeadLoading.tsx";
+import {render, screen} from "@testing-library/react";
+import {Column} from "../../../../src/utils/types.tsx";
+import {expect} from "vitest";
+
+describe("TableHeadLoading Component", () => {
+  const mockColumns: Column[] = [
+    {label: "Select", accessor: "select", sortable: false},
+    {label: "Name", accessor: "name", sortable: true},
+    {label: "Email", accessor: "email", sortable: true},
+    {label: "Role", accessor: "role", sortable: true},
+  ]
+
+  it("renders a table header row", () => {
+    render(<TableHeadLoading columns={mockColumns}/>);
+
+    const headerRow = screen.getByRole("row");
+    expect(headerRow).toBeInTheDocument();
+  });
+
+  it("renders the correct number of header cells", () => {
+    render(<TableHeadLoading columns={mockColumns}/>);
+
+    const headerCells = screen.getAllByRole("columnheader");
+    expect(headerCells).toHaveLength(mockColumns.length);
+  });
+
+  it("renders CheckboxLoadingContainer for the 'select' column", () => {
+    render(<TableHeadLoading columns={mockColumns}/>);
+    screen.debug()
+    const checkbox = screen.getByTestId("checkbox-loading-checkbox-all");
+    expect(checkbox.firstChild).toBeInTheDocument();
+    expect(checkbox.firstChild).toBeDisabled();
+    expect(checkbox.firstChild).not.toBeChecked()
+  });
+
+  it("renders labels for non-'select' columns", () => {
+    render(<TableHeadLoading columns={mockColumns}/>);
+
+    const nameHeader = screen.getByText("Name");
+    const emailHeader = screen.getByText("Email");
+
+    expect(nameHeader).toBeInTheDocument();
+    expect(emailHeader).toBeInTheDocument();
+  });
+});
