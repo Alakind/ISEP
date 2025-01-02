72d166a901979a1fad16101581a8a68c11b91996
JSweers <j.e.sweers@student.utwente.nl>
2025-01-02 16:36:00 +0100

    Test RoleSelectContainer.tsx container

--- frontend/management/src/components/RoleSelect.tsx
+++ frontend/management/src/components/RoleSelect.tsx
@@ -7,9 +7,9 @@ function RoleSelect({id, selectedOption, handleSelect, disabled}: Readonly<Props
     <span className="dropdown" data-testid={"role-select"}>
       {
         disabled ?
-          <span className="dropdown__select dropdown__select--disabled">{selectedOption ? selectedOption.toString() : "-"}</span> :
+          <span className="dropdown__select dropdown__select--disabled" data-testid={"role-select-combobox"}>{selectedOption ? selectedOption.toString() : "-"}</span> :
           <select id={`role-select_${id}`} onChange={(e: ChangeEvent<HTMLSelectElement>): void => handleSelect(e)} className="dropdown__select"
-                  defaultValue={selectedOption ? selectedOption.toString() : "-"}>
+                  defaultValue={selectedOption ? selectedOption.toString() : "-"} data-testid={"role-select-combobox"}>
             <option value={Roles.NO_ACCESS.toString()}>{Roles.NO_ACCESS.toString()}</option>
             <option value={Roles.ADMIN.toString()}>{Roles.ADMIN.toString()}</option>
             <option value={Roles.RECRUITER.toString()}>{Roles.RECRUITER.toString()}</option>
