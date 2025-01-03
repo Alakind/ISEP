9f0db4d0d7075be2b07c86562e0d4460111ba210
JSweers <j.e.sweers@student.utwente.nl>
2025-01-03 03:43:03 +0100

    Test BulkActionSelectContainer.tsx container (could not fully successfully done)

--- frontend/management/src/components/CustomWarnToast.tsx
+++ frontend/management/src/components/CustomWarnToast.tsx
@@ -3,12 +3,12 @@ import {ReactNode} from "react";
 
 function CustomWarnToast({proceedAction, cancelAction, message}: Readonly<Props>): ReactNode {
   return (
-    <div className="custom-warn-toast__container">
+    <div className="custom-warn-toast__container" data-testid="custom-warn-toast">
       {message}
       <br/>
       <span className="custom-warn-toast__btn-container">
-        <button className="btn btn--toast btn--toast--proceed" onClick={proceedAction}>Proceed</button>
-        <button className="btn btn--toast btn--toast--cancel" onClick={cancelAction}>Cancel</button>
+        <button className="btn btn--toast btn--toast--proceed" onClick={proceedAction} data-testid="proceed-button">Proceed</button>
+        <button className="btn btn--toast btn--toast--cancel" onClick={cancelAction} data-testid="cancel-button">Cancel</button>
       </span>
     </div>)
 }
