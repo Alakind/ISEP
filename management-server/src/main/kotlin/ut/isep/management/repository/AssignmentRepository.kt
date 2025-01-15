05d294a58e4160c927ffe4aff13dc994c69a2de6
JSweers <j.e.sweers@student.utwente.nl>
2025-01-15 01:05:09 +0100

    Implement BaseRepository

--- management-server/src/main/kotlin/ut/isep/management/repository/AssignmentRepository.kt
+++ management-server/src/main/kotlin/ut/isep/management/repository/AssignmentRepository.kt
@@ -1,8 +1,7 @@
 package ut.isep.management.repository
 
-import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import ut.isep.management.model.entity.Assignment
 
 @Repository
-interface AssignmentRepository : JpaRepository<Assignment, Long>
+interface AssignmentRepository : BaseRepository<Assignment, Long>
