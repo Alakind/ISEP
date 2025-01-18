76fc3a1d63d6e0c2d6c110646a755ea0b1705e52
JSweers <j.e.sweers@student.utwente.nl>
2025-01-18 18:28:27 +0100

    Add ThemeContext

--- frontend/management/src/containers/header/ThemeSwitchContainer.tsx
+++ frontend/management/src/containers/header/ThemeSwitchContainer.tsx
@@ -1,31 +1,11 @@
-import {useEffect, useState} from 'react';
-import {Themes} from "../../utils/constants.tsx";
 import ThemeSwitch from "../../components/header/ThemeSwitch.tsx";
+import {useTheme} from "../../ThemeContext.tsx";
 
 function ThemeSwitchContainer() {
-
-  const [theme, setTheme] = useState<(typeof Themes)[keyof typeof Themes]>(Themes.DARK);
-
-  useEffect((): void => {
-    const storedTheme: string | null = localStorage.getItem("Theme");
-    if (storedTheme == "LIGHT") {
-      setTheme(Themes.LIGHT);
-    }
-  }, []);
-
-  useEffect((): void => {
-    localStorage.setItem("Theme", theme === Themes.DARK ? "DARK" : "LIGHT");
-    document.querySelector("body")?.setAttribute("data-theme", theme.toString())
-  }, [theme]);
-
-  function switchTheme(): void {
-    setTheme((theme: string): string => (
-      theme === Themes.DARK ? Themes.LIGHT : Themes.DARK
-    ));
-  }
+  const {theme, toggleTheme} = useTheme();
 
   return (
-    <ThemeSwitch switchTheme={switchTheme} theme={theme}/>
+    <ThemeSwitch switchTheme={toggleTheme} theme={theme}/>
   )
 }
 
--- frontend/management/src/main.tsx
+++ frontend/management/src/main.tsx
@@ -5,9 +5,12 @@ import {RouterProvider} from "react-router-dom";
 import "./index.css";
 import "react-toastify/dist/ReactToastify.css";
 import {router} from "./routerConfiguration.tsx";
+import {ThemeProvider} from "./ThemeContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router}/>
+    <ThemeProvider>
+      <RouterProvider router={router}/>
+    </ThemeProvider>
   </StrictMode>
 );
