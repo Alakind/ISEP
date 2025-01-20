515169cfa3a9b9b1bfccfd579d72687efc63aa42
JSweers <j.e.sweers@student.utwente.nl>
2025-01-20 17:42:48 +0100

    Add coding block tests

--- frontend/management/src/components/applicant-personal/results/SolvedAssignmentCoding.tsx
+++ frontend/management/src/components/applicant-personal/results/SolvedAssignmentCoding.tsx
@@ -1,15 +1,14 @@
-import {MouseEvent} from 'react';
 import ReactDiffViewer, {DiffMethod} from "react-diff-viewer-continued";
 import {Themes} from "../../../utils/constants.tsx";
 import {AssignmentCodingSolvedInterface} from "../../../utils/types.tsx";
 import "../../../styles/coding-diff.css"
 import TestResultBlock from "./TestResultBlock.tsx";
 
-function SolvedAssignmentCoding({theme = Themes.LIGHT, assignment, handleShowCodeChanges, showCodeChanges, highlightLine, onLineNumberClick, showTestResults, handleShowTestResults}: Readonly<Props>) {
+function SolvedAssignmentCoding({theme = Themes.LIGHT, assignment, handleShowCodeChanges, showCodeChanges, showTestResults, handleShowTestResults}: Readonly<Props>) {
   return (
     <div className="solved-assignment-coding">
       {
-        assignment.referenceAnswer == undefined || assignment.answer == undefined || assignment.referenceAnswer.answer == "" || assignment.answer.answer == "" || assignment.answer.answer == assignment.referenceAnswer.answer
+        (!assignment.startCode && !assignment.answer?.answer) || assignment.answer?.answer == assignment.startCode
           ? (
             <p>No changes are made to the original code</p>
           ) : (
@@ -24,13 +23,11 @@ function SolvedAssignmentCoding({theme = Themes.LIGHT, assignment, handleShowCod
                 showCodeChanges && (
                   <div className={"coding__diff-viewer"}>
                     <ReactDiffViewer
-                      highlightLines={highlightLine}
-                      onLineNumberClick={onLineNumberClick}
                       extraLinesSurroundingDiff={2}
-                      oldValue={(assignment.answer.answer || assignment.answer) ?? ""}
+                      oldValue={(assignment.startCode) ?? ""}
                       compareMethod={DiffMethod.CHARS}
                       splitView={false}
-                      newValue={(assignment.referenceAnswer.answer || assignment.referenceAnswer) ?? ""}
+                      newValue={(assignment.answer.answer || assignment.answer) ?? ""}
                       useDarkTheme={theme === Themes.DARK}
                     />
 
@@ -61,8 +58,6 @@ interface Props {
   assignment: AssignmentCodingSolvedInterface;
   handleShowCodeChanges: () => void;
   showCodeChanges: boolean;
-  highlightLine: string[];
-  onLineNumberClick: (id: string, e: MouseEvent<HTMLTableCellElement>) => void;
   showTestResults: boolean;
   handleShowTestResults: () => void;
 }
