113a1c960f4c0fb229ab26e730c900c3bba7d97b
JSweers <j.e.sweers@student.utwente.nl>
2025-01-24 19:20:55 +0100

    Fix typo

--- frontend/management/src/utils/msal/UseUserData.tsx
+++ frontend/management/src/utils/msal/UseUserData.tsx
@@ -4,7 +4,7 @@ import {UserContext} from "./UserContext.tsx";
 export const useUserData = () => {
   const context = useContext(UserContext);
   if (!context) {
-    throw new Error("useMsUserData must be used within a MsUserProvider");
+    throw new Error("useUserData must be used within a UserProvider");
   }
   return context;
 };
