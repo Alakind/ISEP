de93b27b2ff356984291ec0fa15cf471108c1490
JSweers <j.e.sweers@student.utwente.nl>
2025-01-25 01:25:17 +0100

    Ensure msalInstance is initialized before render

--- frontend/management/src/main.tsx
+++ frontend/management/src/main.tsx
@@ -12,26 +12,30 @@ import {msalConfig} from "./AuthConfig.ts";
 
 const msalInstance = new PublicClientApplication(msalConfig);
 
-if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
-  msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0])
-}
-
-msalInstance.addEventCallback((event) => {
-  if (event.payload) {
-    const authenticationResult = event.payload as AuthenticationResult;
-    const account = authenticationResult.account;
-    if (event.eventType === EventType.LOGIN_SUCCESS && account) {
-      msalInstance.setActiveAccount(account)
-    }
+msalInstance.initialize().then(() => {
+  if (!msalInstance.getActiveAccount() && msalInstance.getAllAccounts().length > 0) {
+    msalInstance.setActiveAccount(msalInstance.getAllAccounts()[0])
   }
-})
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <MsalProvider instance={msalInstance}>
-      <ThemeProvider>
-        <RouterProvider router={router}/>
-      </ThemeProvider>
-    </MsalProvider>
-  </StrictMode>
-);
+  msalInstance.enableAccountStorageEvents();
+
+  msalInstance.addEventCallback((event) => {
+    if (event.payload) {
+      const authenticationResult = event.payload as AuthenticationResult;
+      const account = authenticationResult.account;
+      if (event.eventType === EventType.LOGIN_SUCCESS && account) {
+        msalInstance.setActiveAccount(account)
+      }
+    }
+  })
+
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <MsalProvider instance={msalInstance}>
+        <ThemeProvider>
+          <RouterProvider router={router}/>
+        </ThemeProvider>
+      </MsalProvider>
+    </StrictMode>
+  );
+})
