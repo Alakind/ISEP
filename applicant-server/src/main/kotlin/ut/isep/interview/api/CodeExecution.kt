81af8e3f77f259c7efce3aa3569c7017a2f1da63
koppenkraker13 <j.h.snoijer@student.utwente.nl>
2025-01-26 13:29:58 +0100

    Shorten implementation

--- applicant-server/src/main/kotlin/ut/isep/interview/api/CodeExecution.kt
+++ applicant-server/src/main/kotlin/ut/isep/interview/api/CodeExecution.kt
@@ -1,5 +1,6 @@
 package ut.isep.interview.api
 
+import jakarta.servlet.http.HttpServletRequest
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,93 +16,53 @@ import java.io.File
 class CodeExecution {
     // Python, C#, SQL, Java, and JavaScript
 
-    @PostMapping(path = ["/{uuid}/java/initialize"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    fun initializeJavaContainer(@PathVariable uuid: String, @RequestPart("file", required = false) file: MultipartFile?): ResponseEntity<Any> {
+    @PostMapping(path = ["/{uuid}/sql/initialize", "/{uuid}/python/initialize", "/{uuid}/java/initialize"],
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
+    fun initializeSQLContainer(@PathVariable uuid: String,
+                               @RequestPart("file", required = false) file: MultipartFile?,
+                               request: HttpServletRequest): ResponseEntity<Any> {
         val containerFile: File
+        val path = request.requestURI
         if (file == null) {
-            containerFile = File("src/main/resources/defaultContainers/JavaDockerfile")
+            containerFile = when {
+                path.contains("/sql/") -> File("src/main/resources/defaultContainers/SQLDockerfile")
+                path.contains("/python/") -> File("src/main/resources/defaultContainers/PythonDockerfile")
+                path.contains("/java/") -> File("src/main/resources/defaultContainers/JavaDockerfile")
+                else -> throw IllegalArgumentException("Unsupported language: $path")
+            }
         } else {
             containerFile = File.createTempFile(file.name, null)
             file.transferTo(containerFile)
         }
 
         try {
-            JavaExecutor.startContainer(uuid, containerFile)
+            when {
+                path.contains("/sql/") -> SQLExecutor.startContainer(uuid, containerFile)
+                path.contains("/python/") -> PythonExecutor.startContainer(uuid, containerFile)
+                path.contains("/java/") -> JavaExecutor.startContainer(uuid, containerFile)
+                else -> return ResponseEntity.badRequest().body("The requested language is not supported.")
+            }
             return ResponseEntity.ok().build()
         } catch (e: Exception) {
             return ResponseEntity.badRequest().body(e.message)
         }
     }
 
-    @PostMapping("/{uuid}/java/test")
-    fun testJavaCode(@PathVariable uuid: String, @RequestBody() codeStrings: Map<String, String>): ResponseEntity<Any> {
+    @PostMapping(path = ["/{uuid}/sql/test", "/{uuid}/python/test", "/{uuid}/java/test"])
+    fun testSQLCode(@PathVariable uuid: String,
+                    @RequestBody() codeStrings: Map<String, String>,
+                    request: HttpServletRequest): ResponseEntity<Any> {
         if (!codeStrings.containsKey("code")) {
             return ResponseEntity.badRequest().body("You must provide code with your request")
         }
+        val path = request.requestURI
         try {
-            val result = JavaExecutor.runTest(uuid, codeStrings["code"]!!, codeStrings["test"])
-            return ResponseEntity.ok().body(result)
-        } catch (e: Exception) {
-            return ResponseEntity.status(400).body(e.message)
-        }
-    }
-
-    @PostMapping(path = ["/{uuid}/python/initialize"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    fun initializePythonContainer(@PathVariable uuid: String, @RequestPart("file", required = false) file: MultipartFile?): ResponseEntity<Any> {
-        val containerFile: File
-        if (file == null) {
-            containerFile = File("src/main/resources/defaultContainers/PythonDockerfile")
-        } else {
-            containerFile = File.createTempFile(file.name, null)
-            file.transferTo(containerFile)
-        }
-
-        try {
-            PythonExecutor.startContainer(uuid, containerFile)
-            return ResponseEntity.ok().build()
-        } catch (e: Exception) {
-            return ResponseEntity.badRequest().body(e.message)
-        }
-    }
-
-    @PostMapping("/{uuid}/python/test")
-    fun testPythonCode(@PathVariable uuid: String, @RequestBody() codeStrings: Map<String, String>): ResponseEntity<Any> {
-        if (!codeStrings.containsKey("code")) {
-            return ResponseEntity.badRequest().body("You must provide code with your request")
-        }
-        try {
-            val result = PythonExecutor.runTest(uuid, codeStrings["code"]!!, codeStrings["test"])
-            return ResponseEntity.ok().body(result)
-        } catch (e: Exception) {
-            return ResponseEntity.status(400).body(e.message)
-        }
-    }
-
-    @PostMapping(path = ["/{uuid}/sql/initialize"], consumes = [MediaType.MULTIPART_FORM_DATA_VALUE])
-    fun initializeSQLContainer(@PathVariable uuid: String, @RequestPart("file", required = false) file: MultipartFile?): ResponseEntity<Any> {
-        val containerFile: File
-        if (file == null) {
-            containerFile = File("src/main/resources/defaultContainers/SQLDockerfile")
-        } else {
-            containerFile = File.createTempFile(file.name, null)
-            file.transferTo(containerFile)
-        }
-
-        try {
-            SQLExecutor.startContainer(uuid, containerFile)
-            return ResponseEntity.ok().build()
-        } catch (e: Exception) {
-            return ResponseEntity.badRequest().body(e.message)
-        }
-    }
-
-    @PostMapping("/{uuid}/sql/test")
-    fun testSQLCode(@PathVariable uuid: String, @RequestBody() codeStrings: Map<String, String>): ResponseEntity<Any> {
-        if (!codeStrings.containsKey("code")) {
-            return ResponseEntity.badRequest().body("You must provide code with your request")
-        }
-        try {
-            val result = SQLExecutor.runTest(uuid, codeStrings["code"]!!, codeStrings["test"])
+            val result = when {
+                path.contains("/sql/") -> SQLExecutor.runTest(uuid, codeStrings["code"]!!, codeStrings["test"])
+                path.contains("/python/") -> PythonExecutor.runTest(uuid, codeStrings["code"]!!, codeStrings["test"])
+                path.contains("/java/") -> JavaExecutor.runTest(uuid, codeStrings["code"]!!, codeStrings["test"])
+                else -> return ResponseEntity.badRequest().body("The requested language is not supported.")
+            }
             return ResponseEntity.ok().body(result)
         } catch (e: Exception) {
             return ResponseEntity.status(400).body(e.message)
