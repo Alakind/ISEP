fef8a74e1c953497a06a3635163249ca63656333
s2516268 <e.a.devree@student.utwente.nl>
2025-01-27 20:19:51 +0100

    use new getPaginated to match a latest=true example for /assessment

--- management-server/src/main/kotlin/ut/isep/management/service/assessment/AssessmentReadService.kt
+++ management-server/src/main/kotlin/ut/isep/management/service/assessment/AssessmentReadService.kt
@@ -1,6 +1,10 @@
 package ut.isep.management.service.assessment
 
+import dto.PaginatedDTO
 import dto.assessment.AssessmentReadDTO
+import org.springframework.data.domain.Example
+import org.springframework.data.domain.ExampleMatcher
+import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Service
 import ut.isep.management.model.entity.Assessment
 import ut.isep.management.repository.AssessmentRepository
@@ -16,5 +20,15 @@ class AssessmentReadService(
         val entity = repository.findByTagAndLatestTrue(tag)
             ?: throw NoSuchElementException("Entity not found")
         return converter.toDTO(entity)
+    }
+
+
+    fun getLatestPaginated(pageable: Pageable): PaginatedDTO<AssessmentReadDTO> {
+        val latestAssessmentExample = Assessment(latest = true)
+        val matcher = ExampleMatcher.matching()
+            .withIgnorePaths(*Assessment::class.java.declaredFields.map { it.name }.filter { it != "latest" }.toTypedArray())
+            .withMatcher("latest", ExampleMatcher.GenericPropertyMatchers.exact())
+
+        return super.getPaginated(Example.of(latestAssessmentExample, matcher), pageable)
     }
 }
