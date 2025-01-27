c7ccd37f9bd8d6025e91e9b5da0cc793cbf634bc
s2516268 <e.a.devree@student.utwente.nl>
2025-01-27 18:28:37 +0100

    update Assignment Read DTOs with reference answers

--- shared-models/src/main/kotlin/dto/assignment/BaseAssignment.kt
+++ shared-models/src/main/kotlin/dto/assignment/BaseAssignment.kt
@@ -1,6 +1,7 @@
 package dto.assignment
 
 import dto.ReadDTO
+import dto.solution.AnswerCreateReadDTO
 import io.swagger.v3.oas.annotations.media.Schema
 
 
@@ -10,6 +11,7 @@ abstract class BaseAssignment : ReadDTO {
     abstract val type: AssignmentType
     abstract val availablePoints: Int
     abstract val availableSeconds: Long
+    abstract val referenceAnswer: AnswerCreateReadDTO?
 
     @Schema(enumAsRef = true)
     enum class AssignmentType { Coding, MultipleChoice, Open }
--- shared-models/src/main/kotlin/dto/assignment/ReferenceAssignmentOpenReadDTO.kt
+++ shared-models/src/main/kotlin/dto/assignment/ReferenceAssignmentOpenReadDTO.kt
@@ -1,13 +1,15 @@
 package dto.assignment
 
+import dto.solution.AnswerCreateReadDTO
 import io.swagger.v3.oas.annotations.media.Schema
 
 @Schema(description = "Open assignment")
-data class AssignmentOpenReadDTO(
+data class ReferenceAssignmentOpenReadDTO(
     override val id: Long,
     override val description: String,
     override val availablePoints: Int,
     override val availableSeconds: Long,
-) : BaseAssignment(), AssignmentReadDTO {
+    override val referenceAnswer: AnswerCreateReadDTO.Open?,
+) : BaseAssignment(), ReferenceAssignmentReadDTO {
     override val type: AssignmentType = AssignmentType.Open
 }
