2c8de605e9ccfb9d95093568f35549a83ad3dae0
JSweers <j.e.sweers@student.utwente.nl>
2025-01-28 21:25:24 +0100

    Update global exception handler

--- management-server/src/main/kotlin/ut/isep/management/exception/GlobalExceptionHandler.kt
+++ management-server/src/main/kotlin/ut/isep/management/exception/GlobalExceptionHandler.kt
@@ -1,9 +1,11 @@
 package ut.isep.management.exception
 
+import org.springframework.data.mapping.PropertyReferenceException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ControllerAdvice
 import org.springframework.web.bind.annotation.ExceptionHandler
+import org.springframework.web.client.HttpClientErrorException
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException
 
 @ControllerAdvice
@@ -12,6 +14,16 @@ class GlobalExceptionHandler {
     @ExceptionHandler(NoSuchElementException::class)
     fun handleNoSuchElementException(ex: NoSuchElementException): ResponseEntity<String> {
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.message)
+    }
+
+    @ExceptionHandler(HttpClientErrorException::class)
+    fun handleHttpClientErrorException(ex: HttpClientErrorException): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.message)
+    }
+
+    @ExceptionHandler(PropertyReferenceException::class)
+    fun handlePropertyReferenceException(ex: PropertyReferenceException): ResponseEntity<String> {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
     @ExceptionHandler(TooManyRegisteredTimedObjectsException::class)
@@ -30,23 +42,22 @@ class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(MethodArgumentTypeMismatchException::class) //includes ConversionFailedException
-    fun handleConversionConflict(ex: RuntimeException): ResponseEntity<String> {
+    fun handleConversionConflict(ex: MethodArgumentTypeMismatchException): ResponseEntity<String> {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
     @ExceptionHandler(NotAllowedUpdateException::class)
-    fun handleNotAllowedUpdateException(ex: RuntimeException): ResponseEntity<String> {
+    fun handleNotAllowedUpdateException(ex: NotAllowedUpdateException): ResponseEntity<String> {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
 
     @ExceptionHandler(IllegalArgumentException::class)
     fun handleIllegalArgumentException(ex: IllegalArgumentException): ResponseEntity<String> {
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.message)
     }
 
     @ExceptionHandler(OidNotUniqueException::class)
-    fun handleOidNotUniqueException(ex: RuntimeException): ResponseEntity<String> {
+    fun handleOidNotUniqueException(ex: OidNotUniqueException): ResponseEntity<String> {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.message)
     }
-
 }
