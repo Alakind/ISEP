a562a89ca94849334acd0a6f55f354132f3f1699
s2516268 <e.a.devree@student.utwente.nl>
2025-02-03 16:13:41 +0100

    Add execution-server SSL cert to RestTemplateConfig

--- management-server/build.gradle.kts
+++ management-server/build.gradle.kts
@@ -29,6 +29,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.4.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.18.0")
     implementation("org.slf4j:slf4j-api")
+    implementation("org.apache.httpcomponents.client5:httpclient5")
     implementation(project(":shared-models"))
     implementation(project(":shared-entities"))
     runtimeOnly("org.postgresql:postgresql")
--- management-server/src/main/kotlin/ut/isep/management/config/RestTemplateConfig.kt
+++ management-server/src/main/kotlin/ut/isep/management/config/RestTemplateConfig.kt
@@ -1,9 +1,16 @@
 package ut.isep.management.config
 
+import org.apache.hc.client5.http.impl.classic.HttpClients
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder
+import org.apache.hc.client5.http.ssl.*
+import org.apache.hc.core5.ssl.SSLContexts
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.ClientHttpRequestInterceptor
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.web.client.RestTemplate
+import java.security.KeyStore
+import java.security.cert.CertificateFactory
 
 @Configuration
 class RestTemplateConfig {
@@ -24,8 +31,31 @@ class RestTemplateConfig {
 
     @Bean(name = ["executorRestTemplate"])
     fun executorRestTemplate(): RestTemplate {
-        val restTemplate = RestTemplate()
-        // Additional configuration for executor-related requests if needed
-        return restTemplate
+        // Path to the self-signed certificate
+        val trustStorePath = "certificates/trust-executor.jks" // Update this path
+        val classLoader = Thread.currentThread().contextClassLoader
+        val trustStorePassword = "changeit"
+        val trustStoreInputStream = classLoader.getResourceAsStream(trustStorePath)
+        val trustStore = KeyStore.getInstance(KeyStore.getDefaultType())
+        trustStore.load(trustStoreInputStream, trustStorePassword.toCharArray())
+        trustStoreInputStream?.close()
+
+        // Create an SSLContext that uses the KeyStore
+        val sslContext = SSLContexts.custom()
+            .loadTrustMaterial(trustStore, null)
+            .build()
+
+        val cm = PoolingHttpClientConnectionManagerBuilder.create()
+            .setSSLSocketFactory(
+                SSLConnectionSocketFactory(sslContext, NoopHostnameVerifier.INSTANCE)
+            )
+            .build()
+
+        val httpClient = HttpClients.custom()
+            .setConnectionManager(cm)
+            .build()
+
+        return RestTemplate(HttpComponentsClientHttpRequestFactory(httpClient))
     }
+
 }
