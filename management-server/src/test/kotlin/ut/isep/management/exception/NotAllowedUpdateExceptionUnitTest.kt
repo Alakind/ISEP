b4ab69fb33efb92c72d16e819dae2d1cab83bb70
JSweers <j.e.sweers@student.utwente.nl>
2025-02-04 23:37:51 +0100

    update imports

--- management-server/src/test/kotlin/ut/isep/management/exception/NotAllowedUpdateExceptionUnitTest.kt
+++ management-server/src/test/kotlin/ut/isep/management/exception/NotAllowedUpdateExceptionUnitTest.kt
@@ -2,13 +2,14 @@ package ut.isep.management.exception
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class NotAllowedUpdateExceptionUnitTest {
 
     @Test
     fun `should throw NotAllowedUpdateException with correct message`() {
         val exceptionMessage = "This update is not allowed"
-        val exception = org.junit.jupiter.api.assertThrows<NotAllowedUpdateException> {
+        val exception = assertThrows<NotAllowedUpdateException> {
             throw NotAllowedUpdateException(exceptionMessage)
         }
 
--- management-server/src/test/kotlin/ut/isep/management/exception/OidNotUniqueExceptionUnitTest.kt
+++ management-server/src/test/kotlin/ut/isep/management/exception/OidNotUniqueExceptionUnitTest.kt
@@ -2,13 +2,14 @@ package ut.isep.management.exception
 
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 
 class OidNotUniqueExceptionUnitTest {
 
     @Test
     fun `should throw OidNotUniqueException with correct message`() {
         val exceptionMessage = "Oid is not unique"
-        val exception = org.junit.jupiter.api.assertThrows<OidNotUniqueException> {
+        val exception = assertThrows<OidNotUniqueException> {
             throw OidNotUniqueException(exceptionMessage)
         }
 
