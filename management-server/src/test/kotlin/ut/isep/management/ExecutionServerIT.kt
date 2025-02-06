3446e40c2b7dc83ff518ee30846f3e5561847731
s2516268 <e.a.devree@student.utwente.nl>
2025-02-06 16:27:37 +0100

    test WebClient calls to applicant-server

--- management-server/src/test/kotlin/ut/isep/management/ExecutionServerIT.kt
+++ management-server/src/test/kotlin/ut/isep/management/ExecutionServerIT.kt
@@ -1,5 +1,6 @@
 package ut.isep.management
 
+import dto.testresult.TestResultCreateReadDTO
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Qualifier
@@ -8,27 +9,44 @@ import org.springframework.context.annotation.PropertySource
 import org.springframework.http.ResponseEntity
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.web.client.RestTemplate
+import org.springframework.web.reactive.function.client.WebClient
 import java.util.*
 
 @ActiveProfiles("integration-test")
 @PropertySource("classpath:application-integration-test.properties")
 class ExecutionServerIT(
     @Qualifier("executorRestTemplate")
-    private val restTemplate: RestTemplate
+    private val restTemplate: RestTemplate,
+    @Qualifier("executorWebClient")
+    private val webClient: WebClient
 ) : ManagementApplicationIT() {
 
     @Value("\${execution.base-url}")
     lateinit var executionURL: String
 
     @Test
-    fun `app one should communicate with app two`() {
-        val appTwoUrl = "$executionURL/code-executor/${UUID.nameUUIDFromBytes(byteArrayOf(0))}/cleanup"
+    fun `management-server should communicate with applicant-server using RestTemplate`() {
+        val executorURL = "$executionURL/code-executor/${UUID.nameUUIDFromBytes(byteArrayOf(0))}/cleanup"
 
         // Sending a POST request without a body
-        val response: ResponseEntity<String> = restTemplate.postForEntity(appTwoUrl, null, String::class.java)
+        val response: ResponseEntity<String> = restTemplate.postForEntity(executorURL, null, String::class.java)
 
         // Assert status code is 200 OK
         assertThat(response.statusCode.value()).isEqualTo(200)
     }
 
+    @Test
+    fun `management-server should communicate with applicant-server using WebClient`() {
+        val executorURL = "/${UUID.nameUUIDFromBytes(byteArrayOf(0))}/cleanup"
+
+        // Sending a POST request without a body and retrieving the response
+        val response = webClient.post()
+            .uri(executorURL)
+            .retrieve()
+            .toBodilessEntity()
+            .block() ?: throw IllegalStateException("Response is null")
+
+        // Assert status code is 200 OK
+        assertThat(response.statusCode.value()).isEqualTo(200)
+    }
 }
