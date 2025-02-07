72c0bf5adb3d30954fe84a2a911492a4740db670
JSweers <j.e.sweers@student.utwente.nl>
2025-02-07 22:47:15 +0100

    update build-gradle to make test work in shared models

--- shared-models/build.gradle.kts
+++ shared-models/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm")
+    id("org.springframework.boot")
 }
 
 group = "ut.isep"
@@ -16,6 +17,10 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-api")
     testImplementation("com.ninja-squad:springmockk:3.1.1")
+    testImplementation("org.springframework.boot:spring-boot-starter-test:3.1.4") {
+        exclude("org.junit.vintage:junit-vintage-engine")
+        exclude("org.mockito:mockito-core")
+    }
     testImplementation(kotlin("test"))
 }
 
