6a826d6c6f02b1330f2aa76efc240378a9c7e741
s2516268 <e.a.devree@student.utwente.nl>
2025-02-10 14:53:46 +0100

    InviteReadService split up checkAccessibilityAssessment function, add async function requestContainerStartup

--- management-server/src/main/kotlin/ut/isep/management/service/invite/InviteReadService.kt
+++ management-server/src/main/kotlin/ut/isep/management/service/invite/InviteReadService.kt
@@ -8,22 +8,38 @@ import jakarta.persistence.criteria.CriteriaBuilder
 import jakarta.persistence.criteria.CriteriaQuery
 import jakarta.persistence.criteria.Root
 import jakarta.transaction.Transactional
+import kotlinx.coroutines.*
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toSet
+import kotlinx.coroutines.reactor.awaitSingle
+import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.data.jpa.domain.Specification
 import org.springframework.stereotype.Service
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitBodyOrNull
+import org.springframework.web.reactive.function.client.awaitExchange
+import parser.question.CodingQuestion
+import reactor.core.publisher.Flux
+import reactor.core.publisher.Mono
 import ut.isep.management.exception.AssessmentTimeExceededException
 import ut.isep.management.exception.UnauthorizedException
+import ut.isep.management.model.entity.AssignmentType
 import ut.isep.management.model.entity.Invite
 import ut.isep.management.repository.BaseRepository
 import ut.isep.management.repository.TimingPerSectionRepository
 import ut.isep.management.service.ReadService
+import ut.isep.management.service.assignment.AsyncAssignmentFetchService
 import ut.isep.management.service.converter.assessment.AssessmentReadConverter
 import ut.isep.management.service.converter.invite.InviteReadConverter
 import ut.isep.management.service.converter.invite.PreInfoReadConverter
 import ut.isep.management.service.timing.TimingPerSectionUpdateService
+import ut.isep.management.util.logger
 import java.time.Duration
 import java.time.OffsetDateTime
 import java.time.ZoneOffset
 import java.util.*
+import kotlin.NoSuchElementException
 
 
 @Transactional
@@ -31,11 +47,15 @@ import java.util.*
 class InviteReadService(
     repository: BaseRepository<Invite, UUID>,
     converter: InviteReadConverter,
+    private val fetchService: AsyncAssignmentFetchService,
+    @Qualifier("executorWebClient") private val webClient: WebClient,
     private val assessmentReadConverter: AssessmentReadConverter,
     private val timingPerSectionRepository: TimingPerSectionRepository,
     private val timingPerSectionUpdateService: TimingPerSectionUpdateService,
     private val preInfoReadConverter: PreInfoReadConverter,
 ) : ReadService<Invite, InviteReadDTO, UUID>(repository, converter) {
+    val log = logger()
+
     fun getAssessmentByInviteId(id: UUID): AssessmentReadDTO {
         val invite = repository.findById(id)
             .orElseThrow { NoSuchElementException("Invite not found for the assessment") }
@@ -50,13 +70,66 @@ class InviteReadService(
         return preInfoReadConverter.toDTO(invite)
     }
 
-    private fun startAssessment(invite: Invite) {
-        if (invite.assessmentStartedAt == null) {
-            val currentTime = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC)
-            invite.assessmentStartedAt = currentTime
-            invite.measuredSecondsPerSection.first().visitedAt = currentTime
-            invite.status = InviteStatus.app_started
-            repository.save(invite)
+    fun startAssessment(id: UUID) {
+        val invite = repository.findById(id)
+            .orElseThrow { NoSuchElementException("Invite not found for the assessment") }
+
+        if (invite.assessmentStartedAt != null) {
+            return
+        }
+        val currentTime = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC)
+        invite.assessmentStartedAt = currentTime
+        invite.measuredSecondsPerSection.first().visitedAt = currentTime
+        invite.status = InviteStatus.app_started
+        repository.save(invite)
+        CoroutineScope(Dispatchers.IO).launch {
+            requestContainerStartup(invite)
+        }
+    }
+
+    private suspend fun getCodingLanguagesForInvite(invite: Invite): Set<String> {
+        val codingAssignments = invite.assessment!!.sections
+            .flatMap { section -> section.assignments.filter { it.assignmentType == AssignmentType.CODING } }
+            .distinct()
+
+        return codingAssignments
+            .asFlow()  // Convert list to Flow for reactive handling
+            .map { assignment ->
+                fetchService.fetchAssignment(assignment, invite.assessment!!.gitCommitHash!!).awaitSingle()
+            }
+            .map { fetchedQuestion -> (fetchedQuestion as CodingQuestion).language.lowercase() }
+            .toSet()// Collect the flow into a List
+    }
+
+    suspend fun requestContainerStartup(invite: Invite) {
+        val languages = getCodingLanguagesForInvite(invite)
+        log.info("Languages to start containers for: $languages")
+
+        coroutineScope {  // Launch concurrent requests within this scope
+            val requests = languages.map { language ->
+                async {
+                    val url = "/${invite.id}/$language/initialize"
+                    log.info("Sending POST request to $url to start up container")
+                    try {
+                        webClient.post()
+                            .uri(url)
+                            .awaitExchange { clientResponse ->
+                                if (!clientResponse.statusCode().is2xxSuccessful) {
+                                    val errorBody = clientResponse.awaitBodyOrNull<String>()
+                                    throw IllegalStateException("Request to $url failed with status: ${clientResponse.statusCode()}, body: $errorBody")
+                                }
+                            }
+                    } catch (e: Exception) {
+                        log.error("Error starting container for language $language", e)
+                        throw e
+                    }
+                }
+            }
+
+            requests.awaitAll()  // Wait for all parallel requests to complete
+            if (requests.isEmpty()) {
+                log.info("No coding questions found, no containers started for invite ${invite.id}")
+            }
         }
     }
 
@@ -73,12 +146,13 @@ class InviteReadService(
         if (invite.status == InviteStatus.cancelled) {
             throw UnauthorizedException("Not authorized to retrieve assessment, invitation has been cancelled")
         }
+    }
 
+    fun checkTimingAssessment(id: UUID) {
+        val invite = repository.findById(id).orElseThrow {
+            NoSuchElementException("No invite with id $id")
+        }
         val currentTime = OffsetDateTime.now().withOffsetSameInstant(ZoneOffset.UTC)
-
-        // At initial retrieval of assessment, assessment is started
-        startAssessment(invite)
-
         val assessmentStartedAt = invite.assessmentStartedAt
             ?: throw IllegalStateException("Assessment start time is not set")
 
@@ -90,9 +164,9 @@ class InviteReadService(
             invite.status = InviteStatus.app_finished
             invite.assessmentFinishedAt = currentTime
 
-            timingPerSectionRepository.save(timingPerSectionUpdateService.setMeasuredSecondsPreviousSection(inviteId, currentTime))
+            timingPerSectionRepository.save(timingPerSectionUpdateService.setMeasuredSecondsPreviousSection(invite.id, currentTime))
             repository.save(invite)
-            throw AssessmentTimeExceededException("Assessment time exceeded for invite ID: $inviteId")
+            throw AssessmentTimeExceededException("Assessment time exceeded for invite ID: ${invite.id}")
         }
     }
 
